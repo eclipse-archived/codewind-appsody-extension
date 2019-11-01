7c9763aefb2341fabf664576e0f5b792669d09da
Andrew Mak <makandre@ca.ibm.com>
2019-11-01 11:03:02 -0400

    Appsody version bump + controller workaround (#55)
    * define commands
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * rename
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * add directive
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * rename
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * controller workaround
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * pulling in 0.4.8
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * backout some change
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>
    
    * back out a change
    
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

--- bin/pull.sh
+++ bin/pull.sh
@@ -3,8 +3,8 @@
 set -e
 set -o pipefail
 
-default_appsody_version=0.4.7
-default_appsody_controller_version=0.2.5
+default_appsody_version=0.4.8
+default_appsody_controller_version=0.2.6
 
 appsody_version=${APPSODY_VERSION}
 appsody_controller_version=${APPSODY_CONTROLLER_VERSION}
